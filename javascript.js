785865006f1847cbb93be7ee44f3d054a5e2cce1
Yesgit-David <davebujok@gmail.com>
2023-03-24 19:57:44 +0000

    Add console.log for randomized computer selection.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("Hey Mate")
+const Rock = 1;
+const Paper = 2;
+const Scissors = 3;
+
+function getComputerChoice() {
+   x = Math.floor(Math.random() * 3) + 1;
+   if (x === 1) {
+    console.log("Rock");
+   } else if (x === 2) {
+    console.log("Paper")
+   } else {
+    console.log("Scissors")
+   }
+   
+}
+
+getComputerChoice();
