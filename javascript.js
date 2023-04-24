0154b0a9f753f37c37d67996ed7df9eb273e8dc9
Yesgit-David <davebujok@gmail.com>
2023-04-24 18:04:00 +0100

    Add scoreboard after game finish

--- javascript.js
+++ javascript.js
@@ -1,20 +1,7 @@
 const values = ["Rock", "Paper", "Scissors"];
-const computerChoice = getComputerChoice();
-const playerChoice = getPlayerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-/*
-function getComputerChoice() {
-   x = Math.floor(Math.random() * 3) + 1;
-   if (x === 1) {
-    console.log("Rock");
-   } else if (x === 2) {
-    console.log("Paper")
-   } else {
-    console.log("Scissors")
-   }
-   return
-}
-*/
 function getComputerChoice() {
     pick = values[Math.floor(Math.random() * values.length)];
     return pick;
@@ -22,25 +9,69 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     playerPick = prompt("Give me your choice.");
-    return playerPick;
+    findFirstLetter = playerPick.charAt(0);
+    capFirstLetter = findFirstLetter.toUpperCase();
+    cutRest = playerPick.substring(1);
+    otherSmall = cutRest.toLowerCase();
+    playerPickCorrect = (`${capFirstLetter}${otherSmall}`);
+    return playerPickCorrect;
+}
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    if (playerChoice === computerChoice) {
+        return "Draw"
+    } else if (playerChoice === "Scissors" && computerChoice === 
+    "Paper" || playerChoice === "Rock" && computerChoice === 
+    "Scissors" || playerChoice === "Paper" && computerChoice === "Rock")  { 
+        return "Win"
+    } else {
+        return "Loose"
+    }
 }
 
 
-
-if (playerChoice === computerChoice) {
-    console.log("Draw");
-} else if (playerChoice === "Scissors" && computerChoice === "Paper" || playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Paper" && computerChoice === "Rock")  {
-    console.log("Win");
-} else {
-    console.log("Loose");
+function keepScore() {
+    switch (playRound()) {
+        case "Win":
+            playerScore = playerScore + 1; 
+            break;
+        case "Loose":
+            computerScore = computerScore + 1;
+            break;
+        case "Draw":
+            playerScore = playerScore;
+            computerScore = computerScore;
+    }
 }
 
+function game() {
+    for (let x = 0; x < 5; x++) {
+        keepScore();
+    }
+    if (playerScore === computerScore) {
+        console.log("Draw!");
+    } else if (playerScore > computerScore) {
+        console.log("You Win!"); 
+    } else {
+        console.log("You loose!");
+    }
+}
+
+game();
+console.log(`You got ${playerScore} points and the computer have ${computerScore}`);
 
 
 
 
 
-/* const Typein = prompt("What's your pick? Rock, paper or scissors?"); */
+
+
+
+
+
 
 
 
